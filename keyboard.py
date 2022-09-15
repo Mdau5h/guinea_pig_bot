f8f941fa4f134a862ca5db2293ae4a2fdfd8e3e1
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2022-09-15 11:38:42 +0300

    keyboard refactor

--- keyboard.py
+++ keyboard.py
@@ -1,4 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 
 btn_new = KeyboardButton('/create_new')
@@ -6,3 +6,11 @@ btn_last = KeyboardButton('/get_last')
 btn_some = KeyboardButton('/get_some')
 
 kb = ReplyKeyboardMarkup(resize_keyboard=True).row(btn_new, btn_last, btn_some)
+
+in_btn_prev = InlineKeyboardButton(text='<<', callback_data='prev_page')
+in_btn_next = InlineKeyboardButton(text='>>', callback_data='next_page')
+
+
+in_kb_first_pg = InlineKeyboardMarkup(row_width=1).row(in_btn_next)
+in_kb = InlineKeyboardMarkup(row_width=1).row(in_btn_prev, in_btn_next)
+in_kb_last_pg = InlineKeyboardMarkup(row_width=1).row(in_btn_prev)
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from aiogram.types import ReplyKeyboardRemove
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from manager import FSM, DBManager
-from keyboard import kb
+from keyboard import *
 
 
 API_TOKEN = config.TOKEN
@@ -50,19 +50,23 @@ async def load_record(message: types.Message, state: FSMContext):
 
 @dp.message_handler(commands=['get_last'])
 async def send_last_record(message: types.Message):
-    record_date, record_body = db.get_last_message()
-    answer = f"Последняя запись от: {record_date}. \n" \
-             f"{record_body}"
+    last_record = db.get_last_message()
+    answer = f"Последняя запись от: {last_record[0]}. \n" \
+             f"{last_record[1]}"
     await message.answer(answer, reply_markup=kb)
     await message.delete()
 
 
 @dp.message_handler(commands=['get_some'])
 async def send_record_list(message: types.Message):
+    list_description = f'В моей памяти вот столько записей: {db.get_messages_count()}. \n' \
+                       f'Выбери номер для просмотра. \n' \
+                       f'Стрелками можешь выбрать страницу. '
+    await message.answer(list_description, reply_markup=ReplyKeyboardRemove())
     msg_list = db.get_message_list()
     answer = '\n'.join([f'{msg[0]}: {msg[1]}' for msg in msg_list])
-    await FSM.msg_list.set()
-    await message.answer(answer, reply_markup=ReplyKeyboardRemove())
+    await message.answer(answer, reply_markup=in_kb_last_pg)
+    # await FSM.msg_list.set()
     await message.delete()
 
 
